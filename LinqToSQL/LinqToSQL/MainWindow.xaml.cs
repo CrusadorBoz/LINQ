d4efe460a57db7145e102ea179bf79e7f187d8c0
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2019-04-27 19:47:02 -0500

    Add files via upload

---
+++ LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace LinqToSQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LinqToSQLDataClassesDataContext dataContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["BoztekConnectionString"].ConnectionString;
+            dataContext = new LinqToSQLDataClassesDataContext(connectionString);
+
+            // InsertLectures();
+            // InsertStudentLectureAssociations();
+            // GetUniversityOfToni();
+            // GetLecturesFromToni();
+            // GetAllStudentsFromYale();
+            // GetAllUniversityiesWithTransgenders();
+            GetAllLecturesfromIllinoisStateUniversity();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University yale = new University();
+            yale.Name = "Yale University";
+            dataContext.Universities.InsertOnSubmit(yale);
+
+            University illinoisStateUniversity = new University();
+            illinoisStateUniversity.Name = "Illinois State University";
+            dataContext.Universities.InsertOnSubmit(illinoisStateUniversity);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudent()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale University"));
+            University illinoisStateUniversity = dataContext.Universities.First(un => un.Name.Equals("Illinois State University"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Carla", Gender = "Female", UniversityId = illinoisStateUniversity.Id = illinoisStateUniversity.Id });
+            students.Add(new Student { Name = "Toni", Gender = "Male", University = yale });
+            students.Add(new Student { Name = "Leyle", Gender = "Female", University = illinoisStateUniversity });
+            students.Add(new Student { Name = "James", Gender = "Transgender", University = yale });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+
+        public void InsertLectures()
+        {
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Math" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "History" });
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Lectures;
+        }
+
+        public void InsertStudentLectureAssociations()
+        {
+            Student Carla = dataContext.Students.First(st => st.Name.Equals("Carla"));
+            Student Toni = dataContext.Students.First(st => st.Name.Equals("Toni"));
+            Student Leyle = dataContext.Students.First(st => st.Name.Equals("Leyle"));
+            Student James = dataContext.Students.First(st => st.Name.Equals("James"));
+
+            Lecture Math = dataContext.Lectures.First(lc => lc.Name.Equals("Math"));
+            Lecture History = dataContext.Lectures.First(lc => lc.Name.Equals("History"));
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Carla, Lecture = Math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Toni, Lecture = Math });
+
+            StudentLecture slToni = new StudentLecture();
+            slToni.Student = Toni;
+            slToni.LectureId = History.Id;
+            dataContext.StudentLectures.InsertOnSubmit(slToni);
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Leyle, Lecture = History });
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.StudentLectures;
+        }
+
+        public void GetUniversityOfToni()
+        {
+            Student Toni = dataContext.Students.First(st => st.Name.Equals("Toni"));
+
+            University TonisUniversity = Toni.University;
+
+            List<University> universities = new List<University>();
+            universities.Add(TonisUniversity);
+
+            MainDataGrid.ItemsSource = universities;
+        }
+
+        public void GetLecturesFromToni()
+        {
+            Student Toni = dataContext.Students.First(st => st.Name.Equals("Toni"));
+
+            var tonisLectures = from sl in Toni.StudentLectures select sl.Lecture;
+
+            MainDataGrid.ItemsSource = tonisLectures;
+        }
+
+        public void GetAllStudentsFromYale()
+        {
+            var studentsFromYale = from student in dataContext.Students
+                                   where student.University.Name == "Yale University"
+                                   select student;
+
+            MainDataGrid.ItemsSource = studentsFromYale;
+        }
+
+        public void GetAllUniversityiesWithTransgenders()
+        {
+            var transgenderUniversities = from student in dataContext.Students
+                                          join university in dataContext.Universities
+                                          on student.University equals university
+                                          where student.Gender == "Transgender"
+                                          select university;
+
+            MainDataGrid.ItemsSource = transgenderUniversities;
+        }
+
+        public void GetAllLecturesfromIllinoisStateUniversity()
+        {
+            var lecturesFromIllinoisStateUniversity = from sl in dataContext.StudentLectures
+                                                      join student in dataContext.Students on sl.StudentId equals student.Id
+                                                      where student.University.Name == "Illinois State University"
+                                                      select sl.Lecture;
+
+            MainDataGrid.ItemsSource = lecturesFromIllinoisStateUniversity;
+        }
+      
+    }
+}
